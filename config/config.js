ca891420adb4fda14f95e04d760a4c1ecd22fbf9
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-05 15:58:55 +0600

    added mongodb database

--- config/config.js
+++ config/config.js
@@ -2,5 +2,8 @@ const dev = {
   app: {
     port: 4040,
   },
+  db: {
+    url: "mongodb+srv://practices:parvez4040@cluster0.gxg0obw.mongodb.net/Simple-Product-Cart",
+  },
 };
 module.exports = dev;
---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const url = require("./config").db.url;
+const chalk = require("chalk");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log(chalk.black.bgGreenBright("MongoDB is connected."));
+  } catch (error) {
+    console.log(
+      chalk.black.bgRedBright("MongoDB is not connected.", +" " + error)
+    );
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
